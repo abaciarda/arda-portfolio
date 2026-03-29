f7c1410173d13b01ff12648eb19e915d1eac85b5
abaciarda <ardaabaci19@gmail.com>
2026-03-29 16:17:01 +0300

    ✨ feat: add Google Analytics (gtag) integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navigation from "./components/Navigation";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -69,6 +70,7 @@ export default function RootLayout({
         <Navigation />
         {children}
       </body>
+      <GoogleAnalytics gaId="G-PC1M1B3MJM" />
     </html>
   );
 }
